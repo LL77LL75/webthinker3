b670708b4543ee683dc9e4b712daca999dc4c261
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-07-24 10:50:24 +0000

    Jul 24, 2026, 10:50 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -76,7 +76,7 @@ function draw() {
     clear();
     player.vel.x = 5;
     if (kb.presses("space") || mouse.presses("left"));
-        
+        player.vel.y = -1
     image(bg,0,0,800,500); // image,x,y,width,height
 }
 
