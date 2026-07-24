d17df180b04e6f03a16d773f3926fddb88b55005
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-07-24 09:57:14 +0000

    Jul 24, 2026, 9:57 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -43,7 +43,7 @@ function setup() {
     startCoordinate = [50,height- TILE_SIZE/2];
     player.x = startCoordinate[0];
     player.y =startCoordinate[1];
-    ground = new ground();
+    ground = new Group();
     ground.tile = "g";
     ground.width = TILE_SIZE;
     ground.h = TILE_SIZE;
