8146d672f0dac4917b9de3ea9ae1092c057863ac
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-07-24 10:09:41 +0000

    Jul 24, 2026, 10:09 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -56,6 +56,7 @@ function setup() {
     ground.collider = "static";
     spikes = new Group();
     spikes.img = loadImage("assets/spike.png")
+    or
     new Tiles(tilemap1,0,0,TILE_SIZE,TILE_SIZE)
 }
 function draw() {
