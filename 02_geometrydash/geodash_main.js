1ea83277933f6abfad94cff4867af544fe9ad4d5
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-07-24 10:47:54 +0000

    Jul 24, 2026, 10:47 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -73,9 +73,9 @@ function setup() {
     new Tiles(tilemap1,0,0,TILE_SIZE,TILE_SIZE)
 }
 function draw() {
+    clear()
     player.vel.x = 5
-    if 
-    clear();
+    if (KeyboardEvent.presses("space"))
     image(bg,0,0,800,500); // image,x,y,width,height
 }
 
