d28b8e5d88cf3dcd29fe66cac6205d6d0a912ca1
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-07-24 09:59:39 +0000

    Jul 24, 2026, 9:59 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -27,7 +27,7 @@ const TILE_SIZE = 50;
 
 
 function preload() {
-    tilemap1 = loadStrings(stages/tiles1.txt)
+    tilemap1 = loadStrings("stages/tiles1.txt");
     box = loadImage("assets/cube.png");
     bg = loadImage("assets/geobg.png");
 
