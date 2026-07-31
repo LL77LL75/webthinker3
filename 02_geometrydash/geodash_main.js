add82a0f623c249faa03183362e19a368ad214b3
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-07-31 10:01:24 +0000

    Jul 31, 2026, 10:01 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -84,7 +84,7 @@ function draw() {
         jumpChance -=1
     }
     if (player.collides(ground) && jumpChance < MAX_JUMP){
-        
+        jumpChance = MAX_JUMP
     }
     image(bg,0,0,800,500); // image,x,y,width,height
 }
