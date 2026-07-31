1d26b76b3763e1d5a3797772a36fd6a56f8ce2bf
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-07-31 10:32:47 +0000

    Jul 31, 2026, 10:32 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -103,7 +103,6 @@ function draw() {
             }
         }
     }
-    if (player.collides(ground) && player.x < ground.x-TILE_SIZE/2 && (player.y > ground.y-TILE_SIZE/2 || player.y < ground.y + TILE_SIZE/2))
     image(bg,0,0,800,500); // image,x,y,width,height
 }
 
