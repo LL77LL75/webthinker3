1f7d8d147c8da1432c562ee564ac9bf7b9f0f1da
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-07-31 10:31:56 +0000

    Jul 31, 2026, 10:31 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -96,7 +96,8 @@ function draw() {
     }
     for (let tile of ground){
         if (player.colliding(tile)){
-            
+            let leftEdge = tile.x - tile.w /2;
+            let left
         }
     }
     if (player.collides(ground) && player.x < ground.x-TILE_SIZE/2 && (player.y > ground.y-TILE_SIZE/2 || player.y < ground.y + TILE_SIZE/2))
