516e827e8bccf679c92a0c201648543631723253
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-07-31 09:57:57 +0000

    Jul 31, 2026, 9:57 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -83,9 +83,9 @@ function draw() {
         // player.rotateTo(player + 359,15);
         jumpChance -=1
     }
-    // if (player.collides(sharp)){
-    //     resetGame()
-    // }
+    if (player.collides()){
+        resetGame()
+    }
     image(bg,0,0,800,500); // image,x,y,width,height
 }
 
