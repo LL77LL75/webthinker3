fb681885e0963c6b58fe3a332910bcf4a0520d09
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-07-31 10:27:51 +0000

    Jul 31, 2026, 10:27 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -38,7 +38,7 @@ function preload() {
 
 function setup() {
     new Canvas(700,600);
-    background();
+    background(250);
     world.gravity.y=32;
     player = new Sprite(50,50,TILE_SIZE,TILE_SIZE);
     player.collider = "dynamic";
