680ebef583675421b3517faa01343276f6aacc7d
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-08-07 10:19:33 +0000

    Aug 7, 2026, 10:19 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -42,7 +42,7 @@ function preload() {
     box = loadImage("assets/cube.png");
     bg = loadImage("assets/geobg.png");
     spikes = loadImage("assets/spike.png");
-    
+    tileMap2 = load
 
 }
 
