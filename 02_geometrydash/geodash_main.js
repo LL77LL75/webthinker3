3cb1faa643df4c8aa61b201f2364da0430c0f2c8
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-08-07 10:02:07 +0000

    Aug 7, 2026, 10:02 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -125,17 +125,6 @@ function draw() {
             jumpChance +=1
         }
     }
-    if (!startGame && (mouse.presses() || kb.presses("space"))){
-        startGame = true;
-        startSprite.visible=false;
-    }
-    else if (!startGame){
-        if (frameCount%60 < 30){
-            startSprite.visible = true;
-        }
-        else {
-            startSprite.visible = false;
-        }
-    }
+   
     image(bg,0,0,800,500); // image,x,y,width,height
 }
