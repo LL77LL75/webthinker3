c279208c97fc82cec2b6de0933641c86b550bd0a
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-08-07 09:50:28 +0000

    Aug 7, 2026, 9:50 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -84,7 +84,7 @@ function setup() {
     finishline.visible = false;
     new Tiles(tilemap1,0,0,TILE_SIZE,TILE_SIZE)
 
-    
+    startSprite = new Sprite
 }
 function resetGame(){
     player.rotation = 0;
