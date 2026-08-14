c7eabe8bbb5ce82224fb849c3d8f930cd181053b
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-08-14 10:31:11 +0000

    Aug 14, 2026, 10:31 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -38,7 +38,7 @@ let passSound;
 let mapUsed;
 
 function preload() {
-    backgroundTrack = createAudio("")
+    backgroundTrack = createAudio("assets/stero-madness")
     startGameImg = loadImage("assets/startgame.png")
     endGameImg = loadImage("assets/clear.png")
     startCoordinate = [TILE_SIZE,height-TILE_SIZE/2]
