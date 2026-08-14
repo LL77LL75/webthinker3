66d136ea8fcdda2c2a51189fc6d37bc3901808a6
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-08-14 10:18:23 +0000

    Aug 14, 2026, 10:18 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -14,7 +14,7 @@ let orbs;
 let tilemap1
 let mapused;
 let player; // player sprite
- // player sprite image
+let playerimg; //player sprite image
 let bg; // background image
 let ground;
 let finishline;
