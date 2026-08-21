374d395d983a6a1e3342a47b9aaa4d2b9e062ecf
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-08-21 10:28:17 +0000

    Aug 21, 2026, 10:28 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -41,7 +41,7 @@ let mapUsed;
 
 function preload() {
     backgroundTrack = createAudio("assets/stereo-madness.mp3");
-    passSound = createAudio("/workspaces/webthinker3/02_geometrydash/assets/game-start.mp3");
+    passSound = createAudio("02_geometrydash/assets/game-start.mp3");
     failSound = createAudio("02_geometrydash/assets/geometry-dash-death-sound.mp3")
     startGameImg = loadImage("assets/startgame.png")
     endGameImg = loadImage("assets/clear.png")
