e348c9732c9b23f31a3ad83e1dd884438d3cabbb
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-08-21 10:36:17 +0000

    Aug 21, 2026, 10:36 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -66,7 +66,7 @@ function loadLevel(){
     if (level === 1){
         new Tiles(tileMap1,0,0,50,50);
         let mapUsed = tileMap1;
-        console.log()
+        console.log("forward 1")
     }
     else if (level === 2){
         new Tiles(tileMap2,0,0,50,50);
