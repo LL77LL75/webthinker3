ff549404ff87ae928e223f39b5c18e5579ed2c12
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-08-28 10:58:06 +0000

    Aug 28, 2026, 10:58 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -50,6 +50,7 @@ function setup() {
     balloon.collider = "rgb(255,0,0)"
     balloon.bounceciness = 1;
     balloon.mass = 2;
+    ball.drag = 1;
     balloon.x = videoW/2;
     balloon.y = videoH/2;
 
