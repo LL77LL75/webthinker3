38a3801c9f11eef54c4f1116860830d68b9c2f6c
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-08-28 10:35:35 +0000

    Aug 28, 2026, 10:35 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -51,11 +51,12 @@ function draw() {
 
         // loop through all detected hands
         for (let i = 0; i < hands.length; i++){
-            let hand = hands[i];
-            for (let j = 0; j < hand.keypoints.length; j++){
-                let keypoint = hand.keypoints[j];
-                circle(keypoint.x,keypoint.y,10); //xpos, ypos, radius
-            } 
+            
+            // let hand = hands[i];
+            // for (let j = 0; j < hand.keypoints.length; j++){
+            //     let keypoint = hand.keypoints[j];
+            //     circle(keypoint.x,keypoint.y,10); //xpos, ypos, radius
+            // } 
         }
     }
 }
