a8545b9a25472e4c761203fbfaf4e9954fb812eb
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-09-11 10:32:09 +0000

    Sep 11, 2026, 10:32 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -109,7 +109,7 @@ function draw() {
         text("A GAME",videoW,videoH);
         //instructions
         textSize(32);
-        fill("")
+        fill("rgb()")
     }
     if (gameStart == true && gameOver != false){
         // Draw webcam video
