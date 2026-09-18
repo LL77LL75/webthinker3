9677624902c7691de00a23cb6b2802edce5a0323
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-09-18 10:15:34 +0000

    Sep 18, 2026, 10:15 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -147,6 +147,7 @@ function draw() {
             fingerTip.visible = false;
         }
     }
+    if (fingerTip.collides(balloon))
     if ((balloon.collides(botWall))){
         gameOver = true;
         fingerTip.visible =false;
