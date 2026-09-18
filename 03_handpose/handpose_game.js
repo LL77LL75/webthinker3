681a2b9b3b8ee5f69247a55321e53d39de5f9fba
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-09-18 10:04:26 +0000

    Sep 18, 2026, 10:04 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -150,7 +150,7 @@ function draw() {
         balloon.collider = "kinematic"
         textAlign(CENTER,CENTER);
         textSize(30);
-        text.fill
+        fill()
         text("press space to restart", width/2,height/2);
     }
 }
