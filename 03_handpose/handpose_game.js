424601fd7f0817632d505a77f5dc715924dab4d0
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-09-18 10:41:59 +0000

    Sep 18, 2026, 10:41 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -47,7 +47,7 @@ function setup() {
     world.gravity.y = 3;
 
     bounceSound=createAudio("assets/LowBoing.mp3");
-    gameOverSound=createAudio("")
+    gameOverSound=createAudio("assets/DunDunn.mp3")
 
     fingerTip = new Sprite();
     fingerTip.diameter = 60;
