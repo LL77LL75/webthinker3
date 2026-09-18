c3ce3681a019bdf7804136d43cb7e191752caaf5
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-09-18 10:38:26 +0000

    Sep 18, 2026, 10:38 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -156,7 +156,7 @@ function draw() {
         text(score,0,0);
     }
     if (cooldown>0){
-        cooldown-=
+        cooldown-=Time.deltatime
     }
     if ((balloon.collides(botWall))){
         gameOver = true;
