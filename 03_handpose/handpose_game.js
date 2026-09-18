0c4e61f417e54125ddbcf8d6c7e7f14927e787dc
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-09-18 10:06:06 +0000

    Sep 18, 2026, 10:06 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -114,6 +114,7 @@ function draw() {
         //instructions
         textSize(32);
         fill("rgb(0,200,0)")
+        if (gameOver == true)
         text("use index finger to bounce ball", width/2, height*0.7)
         text("press space to start", width/2, height*0.8)
     }
