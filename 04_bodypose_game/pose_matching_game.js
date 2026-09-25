88759f67688aecbe9f3c9415d4c3714bca7fcda3
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-09-25 10:03:38 +0000

    Sep 25, 2026, 10:03 AM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -63,7 +63,7 @@ function draw() {
     // Draw the middle line that separates Player 1 and Player 2 areas.
     drawMiddleLine();
     //draw cam video
-    Image(video)
+    Image(video,cameraX,0,cameraWidth,cameraHeight)
 
 }
 
